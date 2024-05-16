67367e03244d3b49ce6b202c9e9a5ca47336bab9
joaquingomez3 <gomezgonzalod46@gmail.com>
2024-05-16 00:54:35 -0300

    Update FilasParque.java

--- src/filasparque/FilasParque.java
+++ src/filasparque/FilasParque.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package filasparque;
 
-/**
- *
- * @author Usuario
- */
+
 public class FilasParque {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         
     }
